2de4ef87110fb6b901610e50b4da8f426deebe90
RodLopezDev <rodrigolopezdev@gmail.com>
2025-09-20 20:00:09 -0500

    fix(app @RodLopezDev) 🐛 Add default region

--- deep_larva_function/src/app/aws.py
+++ deep_larva_function/src/app/aws.py
@@ -1,7 +1,11 @@
+"""
+AWS clients for the deep-larva-server
+"""
+
 import boto3
 from botocore.config import Config
 
 s3 = boto3.client(
     "s3", config=Config(s3={"addressing_style": "path"}, signature_version="s3v4")
 )
-dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
